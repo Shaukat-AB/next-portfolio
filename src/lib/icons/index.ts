4468de59e0ede9f18d5bbe751fd12d7eda20be8d
Shaukat Ali <Shaukatalibans@gmail.com>
2025-02-06 14:14:18 +0500

    Refactor icons and util code in lib folder

---
+++ src/lib/icons/index.ts
@@ -0,0 +1,4 @@
+import { BiLogoGithub, BiMenu } from "react-icons/bi";
+
+export const NavMenu = BiMenu;
+export const GithubIcon = BiLogoGithub;
---
+++ src/lib/util.ts
@@ -0,0 +1,8 @@
+
+export const convertToLowerCase = (link:string) => {
+    return link.replaceAll(" ", "-").toLocaleLowerCase();
+}
+
+export const createReadMeLink = (repoName: string) => {
+    return `https://raw.githubusercontent.com/${process.env.NEXT_PUBLIC_GITHUB_USER}/${repoName}/main/README.md`;
+};
