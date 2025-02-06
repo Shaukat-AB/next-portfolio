267eced8e909e29c03b2eaae6fea4c2f89796a34
Shaukat Ali <Shaukatalibans@gmail.com>
2025-02-06 15:14:34 +0500

    Add ShowProjects component to limit visible projects to 4, Allow expanding to max size

--- src/components/protfolio-section/PortfolioSection.tsx
+++ src/components/protfolio-section/PortfolioSection.tsx
@@ -1,17 +1,14 @@
-import { projects } from "@/data/projects";
-import ProjectCard from "../project-card/ProjectCard";
-import GithubButton from "../hero-section/GithubButton";
+import ShowProjects from "./ShowProjects";
+
+const limitProjectsCount = 4;
 
 const PortfolioSection = () => {
     return (
         <section className="mb-16 text-center">
             <h2 className="sub-title mb-16">Featured Projects</h2>
             <div className="flex flex-col flex-wrap md:flex-row items-center gap-16 justify-between">
-                {projects.map((project) => (
-                    <ProjectCard key={project.title} project={project} />
-                ))}
+                <ShowProjects limit={limitProjectsCount} />
             </div>
-            <GithubButton />
         </section>
     );
 };
---
+++ src/components/protfolio-section/ShowProjects.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { projects as allProjects } from "@/data/projects";
+import { useMemo, useState } from "react";
+import ProjectCard from "../project-card/ProjectCard";
+import GithubButton from "../hero-section/GithubButton";
+
+const ShowProjects = ({ limit = 0 }) => {
+    const [limited, setLimited] = useState(true);
+    const projects = useMemo(() => {
+        return limited ? allProjects.slice(0, limit) : allProjects;
+    }, [limited]);
+
+    return (
+        <>
+            {projects.map((project) => (
+                <ProjectCard key={project.title} project={project} />
+            ))}
+            <div className="w-full">
+                {limited ? (
+                    <button
+                        className="btn-dark mx-auto"
+                        onClick={() => setLimited(false)}
+                    >
+                        Expand more
+                    </button>
+                ) : (
+                    <GithubButton />
+                )}
+            </div>
+        </>
+    );
+};
+
+export default ShowProjects;
