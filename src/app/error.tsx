f7f2518761455a65db69c08fd8d786669ce623d1
Shaukat Ali <Shaukatalibans@gmail.com>
2025-02-06 14:28:30 +0500

    Allow reset when there is an error and fix typos

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,19 +2,24 @@
 
 import Link from "next/link";
 
-const ErrorPage = ({ error, rest }: { error: Error; rest: () => void }) => {
+const ErrorPage = ({ error, reset }: { error: Error; reset: () => void }) => {
     return (
         <section className="bg-white dark:bg-gray-900">
             <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
                 <div className="mx-auto max-w-screen-sm text-center">
-                    <h1 className="main-title mb-4">Something went wrong!</h1>
-                    <p className="sub-title mb-4">{error.message}</p>
-                    <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
-                        Try Refreshing the page or go to the home page.
+                    <h1 className="main-title mb-8">Something went wrong!</h1>
+                    <p className="sub-title mb-5">{error.message}</p>
+                    <button
+                        onClick={() => reset()}
+                        className="btn-primary mb-4"
+                    >
+                        Try Again
+                    </button>
+                    <p className="text-lg font-light text-gray-500 dark:text-gray-400">
+                        <Link href="/" className="link">
+                            or go to the home page.
+                        </Link>
                     </p>
-                    <Link href="/" className="btn-primary">
-                        Back to Homepage
-                    </Link>
                 </div>
             </div>
         </section>
