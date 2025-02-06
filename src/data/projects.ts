ddcb8490bfa617cdb89164cfb3d46de6acdd86d7
Shaukat Ali <Shaukatalibans@gmail.com>
2025-02-06 14:34:29 +0500

    Update project order

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,24 +4,28 @@ import { TProject } from "@/types";
 export const projects: TProject[] = [
     {
         title: "ECommerce MERN",
-        image: "",
+        image: "/images/eshophue.jpg",
         readme: createReadMeLink("eshophue"),
     },
-    { title: "Blog NextJS", image: "", readme: createReadMeLink("next-blog") },
+    {
+        title: "Blog NextJS",
+        image: "/images/next-blog.jpg",
+        readme: createReadMeLink("next-blog"),
+    },
     {
         title: "Movieshowcase ReactJS",
         image: "/images/movie-showcase-ten.vercel.app_.jpg",
         readme: createReadMeLink("movie-showcase"),
     },
     {
-        title: "Shopping cart",
-        image: "/images/shopping-cart-seven-weld.vercel.app_.jpg",
-        readme: createReadMeLink("shopping-cart"),
-    },
-    {
         title: "Espyvid",
         image: "/images/espyvid.vercel.app_.jpg",
         readme: createReadMeLink("espyvid"),
+    },
+    {
+        title: "Shopping cart",
+        image: "/images/shopping-cart-seven-weld.vercel.app_.jpg",
+        readme: createReadMeLink("shopping-cart"),
     },
     {
         title: "Chatweb React Firebase",
