f37327a5fd0d757c396a6306de27d5d8f97edf22
Shaukat Ali <Shaukatalibans@gmail.com>
2025-02-11 14:06:43 +0500

    Update hero text

--- src/components/hero-section/HeroSection.tsx
+++ src/components/hero-section/HeroSection.tsx
@@ -4,14 +4,17 @@ import GithubButton from "./GithubButton";
 const HeroSection = () => {
     return (
         <section className="min-h-[80vh] text-center flex items-center justify-center flex-col mb-16">
-            <h1 className="main-title mb-8">
-                Build Apps Beyond Coding
-            </h1>
+            <h1 className="main-title mb-8">Build Apps Beyond Coding</h1>
+            <p className="mb-4 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+                Hi! my name is Shaukat Ali. I am a full-stack developer
+                specializing in creating dynamic applications using React.js and
+                the MERN stack.
+            </p>
             <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
-                Hi! my name is Shaukat Ali. I am a full-stack website developer
-                specializing in React.js, Front-end development, and the MERN
-                stack. I can develop websites of all sizes. Do you need a
-                website or want to work together? Let's start now.
+                I can create websites of all sizes that meet your needs while
+                delivering performant and responsive user interfaces with
+                exceptional user experiences. Do you need a website or want to
+                work together? Feel free to reach out.
             </p>
             <div className="flex flex-col w-full gap-4 justify-center md:flex-row">
                 <SendEmail />
