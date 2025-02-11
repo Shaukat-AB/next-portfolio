fc0c0561148a4f035fc7e4e7ac2a354c43fa98d5
Shaukat Ali <Shaukatalibans@gmail.com>
2025-02-11 12:25:03 +0500

    Remove unused stlyes

--- src/components/header/NavBar.tsx
+++ src/components/header/NavBar.tsx
@@ -9,7 +9,7 @@ const NavBar = () => {
 
     return (
         <nav className="border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <div>
                 <button
                     type="button"
                     className="btn-icon"
