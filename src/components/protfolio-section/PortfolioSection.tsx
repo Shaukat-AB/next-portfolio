9964869b6e8d0e8766f6032e33aefaec439dd62f
Shaukat Ali <Shaukatalibans@gmail.com>
2025-02-12 13:13:38 +0500

    Adjust and reduce spaces between elements on small screen

--- src/components/protfolio-section/PortfolioSection.tsx
+++ src/components/protfolio-section/PortfolioSection.tsx
@@ -6,7 +6,7 @@ const PortfolioSection = () => {
     return (
         <section className="mb-16 text-center">
             <h2 className="sub-title mb-16">Featured Projects</h2>
-            <div className="flex flex-col flex-wrap md:flex-row items-center gap-16 justify-between">
+            <div className="flex flex-col flex-wrap md:flex-row items-center justify-between">
                 <ShowProjects limit={limitProjectsCount} />
             </div>
         </section>
