a4e0842c1982e212ea3d249ce923c6787f6e3d70
Shaukat Ali <Shaukatalibans@gmail.com>
2025-11-27 12:51:30 +0500

    update: show project title on hover

--- src/components/project-card/ProjectCard.tsx
+++ src/components/project-card/ProjectCard.tsx
@@ -2,22 +2,27 @@ import Link from 'next/link';
 import { convertToLowerCase } from '@/lib/util';
 import ProjectImage from './ProjectImage';
 import { TProject } from '@/types';
+import { useState } from 'react';
 
 const ProjectCard = ({ project }: { project: TProject }) => {
+  const [hover, setHover] = useState(false);
+
   return (
     <div className="w-full mb-12 max-w-md mx-auto">
       <Link
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
         href={`projects/${convertToLowerCase(project?.title || '')}`}
         className="relative flex flex-col items-center bg-gray-100 filter border border-border-gray rounded-lg shadow md:flex-row hover:brightness-90 hover:bg-gray-200 transition-all"
       >
-        <div className="relative w-full aspect-[2/1] md:flex-1 max-h-80">
+        <div className="relative w-full aspect-[2/1] md:flex-1">
           <ProjectImage
             project={project}
             className="rounded-t-lg md:rounded-none md:rounded-s-lg"
           />
         </div>
-        {!project?.image && (
-          <span className="bg-gradient-to-b from-transparent to-black/30 text-xl absolute bottom-0 py-4 text-center w-full">
+        {hover && (
+          <span className="text-white font-bold transition bg-gradient-to-b from-transparent to-slate-900/90 text-xl absolute top-0 flex items-end justify-center py-4 text-center w-full h-full">
             {project?.title}
           </span>
         )}
